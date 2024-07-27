c3cbcee5a15c3b943632832202190f977dcd65d9
root <21btrcs228@jainuniversity.ac.in>
2024-07-27 17:39:48 +0000

    exported function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,7 +2,7 @@
  * Returns a promise that logs a message.
  * @returns {Promise<void>} A promise that logs 'I am promise'.
  */
-function getResponseFromAPI() {
+export function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     console.log('I am promise');
     resolve();

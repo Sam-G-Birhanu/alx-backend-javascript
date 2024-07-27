f85b6b72b5d6a0e42a3e0c7966710ca5484c6d6c
root <21btrcs228@jainuniversity.ac.in>
2024-07-27 19:04:18 +0000

    linting

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
-export default function handleResponseFromAPI(promise){
-  return promise.
-    then(() => ({
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
       status: 200,
-      body: success
-    })).
-    catch(() => Error()).
-    finally(
-      console.log('Got a response from the API'))
-};
+      body: success,
+    }))
+    .catch(() => Error())
+    .finally(
+      console.log('Got a response from the API'),
+    );
+}

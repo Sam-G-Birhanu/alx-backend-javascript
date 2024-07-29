04f042ad5301d604a7662f67f3677ed4a9a09d0d
root <21btrcs228@jainuniversity.ac.in>
2024-07-29 13:09:09 +0000

    linting

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,11 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return uploadPhoto()
-    .then((photo) => {
-      console.log(photo.body);
-      return createUser();
-    })
-    .then((user) => {
-      console.log(user.firstName, user.lastName);
+    .then((photo) => createUser().then((user) => ({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      image: photo.body,
+    })))
+    .then((userProfile) => {
+      console.log(userProfile.image, userProfile.firstName, userProfile.lastName);
     });
 }

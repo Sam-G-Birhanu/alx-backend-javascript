5bd0b41a38cd6f7fea258438c5961b1454c9e7e7
root <21btrcs228@jainuniversity.ac.in>
2024-07-29 18:58:10 +0000

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,12 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    queue.push(mathFunction());
-  } catch (e) {
-    queue.push(e.toString());
-  } finally {
-    console.log('Guardrail was processed');
+  const array = []; // create an empty array
+
+  try { // try to run the function and push the result to the array
+    array.push(mathFunction());
+  } catch (err) { // if the function throws an error, push the error to the array
+    array.push(err.toString());
+  } finally { // always push the finally result to the array
+    array.push('Guardrail was processed');
   }
-  return queue;
+  return array; // return the array
 }

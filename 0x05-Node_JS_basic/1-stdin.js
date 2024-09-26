a5ea8aafef6f6630c17934cbe85a164ebd3b2722
root <21btrcs228@jainuniversity.ac.in>
2024-09-26 09:51:24 +0000

    fixed linting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-console.log("Welcome to Holberton School, what is your name?")
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
-process.stdin.on('readable', function() {
-    var name = process.stdin.read();
-    process.stdout.write("Your name is:" + name);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is:${name}`);
 });

2e3a992337a43a97ccfb35f4fb16129cc47b0292
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-09-26 15:54:34 +0530

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is:${name}`);
+  console.log(Your name is:${name});
   process.on('SIGINT', function() {
   console.log('This important software is now closing');
 });

842f78ebd46ef88584df0596435245c6ef895280
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-09-26 15:45:02 +0530

    added exit message

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,4 +3,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   process.stdout.write(`Your name is:${name}`);
+  process.on('SIGINT', function() {
+  console.log('This important software is now closing');
+});
 });

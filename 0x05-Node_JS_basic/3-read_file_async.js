ce5757f8cc3aeab4af14f368307313012b34d778
root <21btrcs228@jainuniversity.ac.in>
2024-09-27 09:36:51 +0000

    fixed linting

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -25,7 +25,6 @@ async function countStudents(path) {
   console.log(`Number of students: ${students.length}`);
   console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
   console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  
 }
 
 module.exports = countStudents;

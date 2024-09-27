c7f5ba237cb8e1ee67ef8857f04bbd642225e444
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-09-27 16:39:57 +0530

    Update 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -5,26 +5,28 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 1245;
 
+// Route for the root URL
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 
+// Route for the /students URL
 app.get('/students', async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  await students(process.argv[2]).then((data) => {
+  res.status(200).send('This is the list of our students\n');
+  try {
+    const data = await students(process.argv[2]); // Call the students function
     res.write(`Number of students: ${data.students.length}\n`);
     res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
-  }).catch((err) => res.write(err.message))
-    .finally(() => {
-      res.end();
-    });
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}\n`);
+    res.end(); // End the response after writing all data
+  } catch (err) {
+    res.status(500).send(err.message); // Send error message and set status code
+  }
 });
 
+// Start the server
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
+
+module.exports = app;

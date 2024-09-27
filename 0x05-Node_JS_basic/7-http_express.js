3eb5d2e72b976623881137805bcffaadf9f9b862
root <21btrcs228@jainuniversity.ac.in>
2024-09-27 12:08:09 +0000

    7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const students = require('./3-read_file_async');
+
 const app = express();
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -25,6 +26,6 @@ app.get('/students', async (req, res) => {
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 module.exports = app;

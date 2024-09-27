791de86bfaf31d57eaa1feb78f2b0a5fde7e1c57
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-09-27 16:04:51 +0530

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -25,6 +25,7 @@ async function countStudents(path) {
   console.log(`Number of students: ${students.length}`);
   console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
   console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  return { students, csStudents, sweStudents };
 }
 
 module.exports = countStudents;

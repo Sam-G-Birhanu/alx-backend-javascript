8b1134b523fd330c4fa9b3c703ba2118c69989fb
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-11-12 19:19:01 +0530

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,10 @@
 export default HolbertonCourse {
   Constructor(name, length, students){
-  this.name = name;
-    this.length = length;
-    this.students = students;
+    if (typeof(name) === string)
+      this._name = name;
+    if (typeof(length) === number)
+      this._length = length;
+    if (typeof(students) === object)
+      this._students = students;
+}
 }

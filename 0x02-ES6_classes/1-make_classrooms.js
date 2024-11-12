504298f8a5f9c4347ee34f6361b6e73ae9d2ba96
Samrawit Guangul <110263254+Sam-G-Birhanu@users.noreply.github.com>
2024-11-12 18:40:09 +0530

    Update 1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+function initializeRooms(){
+  const ClassArray = []
+  ClassArray.append(ClassRoom(19));
+  ClassArray.append(ClassRoom(20));
+  ClassArray.append(ClassRoom(34));
+  return ClassArray;
+}
